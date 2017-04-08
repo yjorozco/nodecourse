5a3afb84753e965787f08af351ff97f7e79c1508
unknown <yamiloro@gmail.com>
2017-04-07 20:41:59 -0430

    addi ng  new feCHURES

---
+++ rest-server-passport2/routes/promoRoute.js
@@ -0,0 +1,132 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Verify = require('./verify');
+var Promotions = require('../models/promotions');
+
+var promotionRouter = express.Router();
+promotionRouter.use(bodyParser.json());
+
+promotionRouter.route('/')
+.get(Verify.verifyOrdinaryUser,  Verify.needsGroup("user"), function (req, res, next) {
+    Promotions.find({}, function (err, promotion) {
+        if (err) throw err;
+        res.json(promotion);
+    });
+})
+
+.post(Verify.verifyOrdinaryUser,  Verify.needsGroup("admin"), function (req, res, next) {
+    Promotions.create(req.body, function (err, promotion) {
+        if (err) throw err;
+        console.log('promotion created!');
+        var id = promotion._id;
+
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Added the promotion with id: ' + id);
+    });
+})
+
+.delete(Verify.verifyOrdinaryUser,  Verify.needsGroup("admin"), function (req, res, next) {
+    Promotions.remove({}, function (err, resp) {
+        if (err) throw err;
+        res.json(resp);
+    });
+});
+
+promotionRouter.route('/:promotionId')
+.get(Verify.verifyOrdinaryUser,  Verify.needsGroup("user"), function (req, res, next) {
+    Promotions.findById(req.params.promotionId, function (err, promotion) {
+        if (err) throw err;
+        res.json(promotion);
+    });
+})
+
+.put(Verify.verifyOrdinaryUser,  Verify.needsGroup("admin"), function (req, res, next) {
+    Promotions.findByIdAndUpdate(req.params.promotionId, {
+        $set: req.body
+    }, {
+        new: true
+    }, function (err, promotion) {
+        if (err) throw err;
+        res.json(promotion);
+    });
+})
+
+.delete(Verify.verifyOrdinaryUser,  Verify.needsGroup("admin"), function (req, res, next) {
+    Promotions.findByIdAndRemove(req.params.promotionId, function (err, resp) {        if (err) throw err;
+        res.json(resp);
+    });
+});
+
+promotionRouter.route('/:promotionId/comments')
+.get(Verify.verifyOrdinaryUser,  Verify.needsGroup("user"), function (req, res, next) {
+    Promotions.findById(req.params.promotionId, function (err, promotion) {
+        if (err) throw err;
+        res.json(promotion.comments);
+    });
+})
+
+.post(Verify.verifyOrdinaryUser,  Verify.needsGroup("admin"), function (req, res, next) {
+    Promotions.findById(req.params.promotionId, function (err, promotion) {
+        if (err) throw err;
+        promotion.comments.push(req.body);
+        promotion.save(function (err, promotion) {
+            if (err) throw err;
+            console.log('Updated Comments!');
+            res.json(promotion);
+        });
+    });
+})
+
+.delete(Verify.verifyOrdinaryUser,  Verify.needsGroup("admin"), function (req, res, next) {
+    Promotions.findById(req.params.promotionId, function (err, promotion) {
+        if (err) throw err;
+        for (var i = (promotion.comments.length - 1); i >= 0; i--) {
+            promotion.comments.id(promotion.comments[i]._id).remove();
+        }
+        promotion.save(function (err, result) {
+            if (err) throw err;
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            res.end('Deleted all comments!');
+        });
+    });
+});
+
+promotionRouter.route('/:promotionId/comments/:commentId')
+.get(Verify.verifyOrdinaryUser,  Verify.needsGroup("user"), function (req, res, next) {
+    Promotions.findById(req.params.promotionId, function (err, promotion) {
+        if (err) throw err;
+        res.json(promotion.comments.id(req.params.commentId));
+    });
+})
+
+.put(Verify.verifyOrdinaryUser,  Verify.needsGroup("admin"), function (req, res, next) {
+    // We delete the existing commment and insert the updated
+    // comment as a new comment
+    Promotions.findById(req.params.promotionId, function (err, promotion) {
+        if (err) throw err;
+        promotion.comments.id(req.params.commentId).remove();
+        promotion.comments.push(req.body);
+        promotion.save(function (err, promotion) {
+            if (err) throw err;
+            console.log('Updated Comments!');
+            res.json(promotion);
+        });
+    });
+})
+
+.delete(Verify.verifyOrdinaryUser,  Verify.needsGroup("admin"), function (req, res, next) {
+    Promotions.findById(req.params.promotionId, function (err, promotion) {
+        promotion.comments.id(req.params.commentId).remove();
+        promotion.save(function (err, resp) {
+            if (err) throw err;
+            res.json(resp);
+        });
+    });
+});
+
+module.exports = promotionRouter;
