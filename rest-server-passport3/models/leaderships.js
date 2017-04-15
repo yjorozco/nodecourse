f8c455d950ec1dade8a047f32812dbe68c93ed31
unknown <yamiloro@gmail.com>
2017-04-14 20:34:43 -0430

    adding new feachures

---
+++ rest-server-passport3/models/leaderships.js
@@ -0,0 +1,46 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// Will add the Currency type to the Mongoose Schema types
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+// create a schema
+var leadershipsSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    designation: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    abbr: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+        unique: true
+    }
+}, {
+    timestamps: true
+});
+
+
+
+// the schema is useless so far
+// we need to create a model using it
+var Leaderships = mongoose.model('Leaderships', leadershipsSchema);
+
+// make this available to our Node applications
+module.exports = Leaderships;
