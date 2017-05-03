02538cadf442d5327c533ce92b96671e7c179b56
yjorozco <yjorozco@cnti.gob.ve>
2017-05-03 10:03:44 -0430

    adding new feachures

---
+++ assigment3/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '226489617837426',
+        clientSecret: '765cb406783802bd4653e58708e5c407',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
+}
