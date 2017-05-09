fa7b0af3a7264cc7b0669465911d421262d623b0
unknown <yamiloro@gmail.com>
2017-05-08 23:33:13 -0430

    adding new feachures

---
+++ assigment4/routes/leaderRoute.js
@@ -0,0 +1,132 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Verify = require('./verify');
+var LeaderShips = require('../models/leaderships');
+
+var leaderShipRouter = express.Router();
+leaderShipRouter.use(bodyParser.json());
+
+leaderShipRouter.route('/')
+.get(Verify.verifyOrdinaryUser,  Verify.needsGroup("user"), function (req, res, next) {
+    LeaderShips.find({}, function (err, leaderShip) {
+        if (err) throw err;
+        res.json(leaderShip);
+    });
+})
+
+.post(Verify.verifyOrdinaryUser,  Verify.needsGroup("admin"), function (req, res, next) {
+    LeaderShips.create(req.body, function (err, leaderShip) {
+        if (err) throw err;
+        console.log('leaderShip created!');
+        var id = leaderShip._id;
+
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Added the leaderShip with id: ' + id);
+    });
+})
+
+.delete(Verify.verifyOrdinaryUser,  Verify.needsGroup("admin"), function (req, res, next) {
+    LeaderShips.remove({}, function (err, resp) {
+        if (err) throw err;
+        res.json(resp);
+    });
+});
+
+leaderShipRouter.route('/:leaderShipId')
+.get(Verify.verifyOrdinaryUser,  Verify.needsGroup("user"), function (req, res, next) {
+    LeaderShips.findById(req.params.leaderShipId, function (err, leaderShip) {
+        if (err) throw err;
+        res.json(leaderShip);
+    });
+})
+
+.put(Verify.verifyOrdinaryUser,  Verify.needsGroup("admin"), function (req, res, next) {
+    LeaderShips.findByIdAndUpdate(req.params.leaderShipId, {
+        $set: req.body
+    }, {
+        new: true
+    }, function (err, leaderShip) {
+        if (err) throw err;
+        res.json(leaderShip);
+    });
+})
+
+.delete(Verify.verifyOrdinaryUser,  Verify.needsGroup("admin"), function (req, res, next) {
+    LeaderShips.findByIdAndRemove(req.params.leaderShipId, function (err, resp) {        if (err) throw err;
+        res.json(resp);
+    });
+});
+
+leaderShipRouter.route('/:leaderShipId/comments')
+.get(Verify.verifyOrdinaryUser,  Verify.needsGroup("user"), function (req, res, next) {
+    LeaderShips.findById(req.params.leaderShipId, function (err, leaderShip) {
+        if (err) throw err;
+        res.json(leaderShip.comments);
+    });
+})
+
+.post(Verify.verifyOrdinaryUser,  Verify.needsGroup("admin"), function (req, res, next) {
+    LeaderShips.findById(req.params.leaderShipId, function (err, leaderShip) {
+        if (err) throw err;
+        leaderShip.comments.push(req.body);
+        leaderShip.save(function (err, leaderShip) {
+            if (err) throw err;
+            console.log('Updated Comments!');
+            res.json(leaderShip);
+        });
+    });
+})
+
+.delete(Verify.verifyOrdinaryUser,  Verify.needsGroup("admin"), function (req, res, next) {
+    LeaderShips.findById(req.params.leaderShipId, function (err, leaderShip) {
+        if (err) throw err;
+        for (var i = (leaderShip.comments.length - 1); i >= 0; i--) {
+            leaderShip.comments.id(leaderShip.comments[i]._id).remove();
+        }
+        leaderShip.save(function (err, result) {
+            if (err) throw err;
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            res.end('Deleted all comments!');
+        });
+    });
+});
+
+leaderShipRouter.route('/:leaderShipId/comments/:commentId')
+.get(Verify.verifyOrdinaryUser,  Verify.needsGroup("user"), function (req, res, next) {
+    LeaderShips.findById(req.params.leaderShipId, function (err, leaderShip) {
+        if (err) throw err;
+        res.json(leaderShip.comments.id(req.params.commentId));
+    });
+})
+
+.put(Verify.verifyOrdinaryUser,  Verify.needsGroup("admin"), function (req, res, next) {
+    // We delete the existing commment and insert the updated
+    // comment as a new comment
+    LeaderShips.findById(req.params.leaderShipId, function (err, leaderShip) {
+        if (err) throw err;
+        leaderShip.comments.id(req.params.commentId).remove();
+        leaderShip.comments.push(req.body);
+        leaderShip.save(function (err, leaderShip) {
+            if (err) throw err;
+            console.log('Updated Comments!');
+            res.json(leaderShip);
+        });
+    });
+})
+
+.delete(Verify.verifyOrdinaryUser,  Verify.needsGroup("admin"), function (req, res, next) {
+    LeaderShips.findById(req.params.leaderShipId, function (err, leaderShip) {
+        leaderShip.comments.id(req.params.commentId).remove();
+        leaderShip.save(function (err, resp) {
+            if (err) throw err;
+            res.json(resp);
+        });
+    });
+});
+
+module.exports = leaderShipRouter;
---
+++ assigment4/routes/verify.js
@@ -0,0 +1,111 @@
+var User = require('../models/user');
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('../config.js');
+
+exports.getToken = function (user) {
+    return jwt.sign(user, config.secretKey, {
+        expiresIn: 3600
+    });
+};
+
+exports.verifyOrdinaryUser = function (req, res, next) {
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+        // verifies secret and checks exp
+        jwt.verify(token, config.secretKey, function (err, decoded) {
+            if (err) {
+                var err = new Error('You are not authenticated!');
+                err.status = 401;
+                return next(err);
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        // if there is no token
+        // return an error
+        var err = new Error('No token provided!');
+        err.status = 403;
+        return next(err);
+    }
+
+};
+
+exports.verifyAdmin = function (req, res, next) {
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+        // verifies secret and checks exp
+        jwt.verify(token, config.secretKey, function (err, decoded) {
+            if (err) {
+                var err = new Error('You are not authenticated!');
+                err.status = 401;
+                return next(err);
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                User.findOne({username: req.decoded._doc.username}, function(err, user) {
+                    if (err) {
+                        return next(err);
+                    } else {                       
+                        if(user.admin)                         
+                             next();
+                         else{
+                            var err = new Error('forbidden');
+                            err.status = 403;
+                            return next(err);
+                         }
+                    }
+                });              
+            }
+        });
+    } else {
+        // if there is no token
+        // return an error
+        var err = new Error('No token provided!');
+        err.status = 403;
+        return next(err);
+    }
+
+};
+
+exports.needsGroup = function(group) {
+  return function(req, res, next) {
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    jwt.verify(token, config.secretKey, function (err, decoded) {
+            if (err) {
+                var err = new Error('You are not authenticated!');
+                err.status = 401;
+                return next(err);
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;                
+            }
+    });
+    User.findOne({username: req.decoded._doc.username}, function(err, user) {
+        if (err) {
+            return next(err);
+        } else {
+            if((user.admin&&group==="admin")||(!user.admin&&group==="user"))                         
+                 next();
+             else{
+                var err = new Error('forbidden');
+                err.status = 403;
+                return next(err);
+             }
+        }
+    });
+  };
+};
+
+exports.getUser = function(token) {  
+    var decoded=jwt.verify(token, config.secretKey);       
+    return decoded._doc;       
+};
